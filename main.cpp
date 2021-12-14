ac5485d0023cd53243f5c29f7884815a3f1e8988
jeevanx <90121218+jeevanx@users.noreply.github.com>
2021-12-14 10:46:03 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include "BigNumber.h"
+
+using namespace std;
+
+
+int bigFact(int n,BigNumber &ans) {
+    BigNumber
+    tmpF;
+    ans.set(1);
+    
+ while{
+        tmpF.set(m);
+        
+        
+    }
+}
+
+int bigPow(int base, int exp, BigNumber &ans){
+    BigNumber
+    tmpBase;
+
+    
+    ans.set(1);
+    tmpBase.set(base);
+    
+    While True{
+        ans.mul2(tmpBase);
+            }
+}
+
+
+
+void bigChoose(int n,int m, BigNumber &ans){
+    BigNumber tmp;
+    
+    if(m>n){
+        ans.set(0);
+        return;
+    }
+    
+    ans.set(1);
+    
+    if(m> n-m)
+        m = n-m;
+       
+    ans.mul2(tmp);
+    ans.divide(den);
+
+    }
+
+}
+
+int main() {
+
+    int64
+    ans, tmp;
+
+    
+    bigFact(8,ans);
+
+    bigPow(3,7,tmp);
+
+    ans.mul2(tmp);
+
+    bigFact(12,tmp);
+
+    ans.mul2(tmp);
+    bigPow(2,10,tmp);
+    ans.mul2(tmp);
+    return 0;
+}
